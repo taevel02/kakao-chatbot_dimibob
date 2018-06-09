bc3834ab284d6f1ba42154ba0008bf7cfa4824dc
Taehoon <kth020315@naver.com>
2018-06-09 10:19:43 +0900

    json parsing

--- html/functions/meals.php
+++ html/functions/meals.php
@@ -1,93 +1,72 @@
 <?php
-    function getmeal1($days) {
-      $date = date("Y-m-d", strtotime("+$days days"));
-
-      header("Content-type: application/json; charset=UTF-8");        // json type and UTF-8 encoding
+    function getmeal1() {
+      $date = date("Y-m-d");
+      header("Content-type: application/json; charset=UTF-8"); // json type and UTF-8 encoding
       $URL = "https://api.dimigo.in/dimibobs/" . $date; // DOMDocument
-      // url 생성
 
       $json = file_get_contents($URL);
-      $result_json = json_decode($json, true);
+      $result_json = json_decode($json);
 
-      $bf = $result_json[breakfast];
-      // $ln = $result_json[lunch];
-      // $dn = $result_json[dinner];
-      // $sn = $result_json[snack];
+      $bf = $result_json->breakfast; // 아침 파싱
 
-      if ( strcmp($bf, '') == false ){
+      if ( strcmp($bf, '') == false ) {
         $bf = "급식 정보가 없습니다."; // 급식이 없을 경우
       }
 
-      $return = $bf;
-      return $return;
+      return $bf;
     }
 
-    function getmeal2($days) {
-      $date = date("Y-m-d", strtotime("+$days days"));
 
-      header("Content-type: application/json; charset=UTF-8");        // json type and UTF-8 encoding
+    function getmeal2() {
+      $date = date("Y-m-d");
+      header("Content-type: application/json; charset=UTF-8"); // json type and UTF-8 encoding
       $URL = "https://api.dimigo.in/dimibobs/" . $date; // DOMDocument
-      // url 생성
 
       $json = file_get_contents($URL);
-      $result_json = json_decode($json, true);
+      $result_json = json_decode($json);
 
-      // $bf = $result_json[breakfast];
-      $ln = $result_json[lunch];
-      // $dn = $result_json[dinner];
-      // $sn = $result_json[snack];
+      $ln = $result_json->lunch; // 점심 파싱
 
-      if ( strcmp($ln, '') == false ){
+      if ( strcmp($ln, '') == false ) {
         $final = "급식 정보가 없습니다."; // 급식이 없을 경우
       }
 
-      $return = $ln;
-      return $return;
+      return $ln;
     }
 
-    function getmeal3($days) {
-      $date = date("Y-m-d", strtotime("+$days days"));
 
-      header("Content-type: application/json; charset=UTF-8");        // json type and UTF-8 encoding
+    function getmeal3() {
+      $date = date("Y-m-d");
+      header("Content-type: application/json; charset=UTF-8"); // json type and UTF-8 encoding
       $URL = "https://api.dimigo.in/dimibobs/" . $date; // DOMDocument
-      // url 생성
 
       $json = file_get_contents($URL);
-      $result_json = json_decode($json, true);
+      $result_json = json_decode($json);
 
-      // $bf = $result_json[breakfast];
-      // $ln = $result_json[lunch];
-      $dn = $result_json[dinner];
-      // $sn = $result_json[snack];
+      $dn = $result_json->dinner; // 저녁 파싱
 
-      if ( strcmp($dn, '') == false ){
+      if ( strcmp($dn, '') == false ) {
         $dn = "급식 정보가 없습니다."; // 급식이 없을 경우
       }
 
-      $return = $dn;
-      return $return;
+      return $dn;
     }
 
-    function getmeal4($days) {
-      $date = date("Y-m-d", strtotime("+$days days"));
 
-      header("Content-type: application/json; charset=UTF-8");        // json type and UTF-8 encoding
+    function getmeal4() {
+      $date = date("Y-m-d");
+      header("Content-type: application/json; charset=UTF-8"); // json type and UTF-8 encoding
       $URL = "https://api.dimigo.in/dimibobs/" . $date; // DOMDocument
-      // url 생성
 
       $json = file_get_contents($URL);
-      $result_json = json_decode($json, true);
+      $result_json = json_decode($json);
 
-      // $bf = $result_json[breakfast];
-      // $ln = $result_json[lunch];
-      // $dn = $result_json[dinner];
-      $sn = $result_json[snack];
+      $sn = $result_json->snack; // 간식 파싱
 
-      if ( strcmp($sn, '') == false ){
+      if ( strcmp($sn, '') == false ) {
         $sn = "급식 정보가 없습니다."; // 급식이 없을 경우
       }
 
-      $return = $sn;
-      return $return;
+      return $sn;
     }
 ?>
--- html/message.php
+++ html/message.php
@@ -1,42 +1,44 @@
 <?php
-    include("functions/echoKakao.php");
-    include("functions/meals.php");
     $data = json_decode(file_get_contents('php://input'), true);
     $content = $data["content"];
 
+    $date = date("Y.m.d");
+
+    include("functions/echoKakao.php");
+    include("functions/meals.php");
+
     if ( strcmp($content, "아침") == false ) {
-        $final = getmeal1(0);
-        $date = date("Y-m-d", strtotime("+$days days"));
+        $final = getmeal1();
         start_echo();
             start_msg();
-                echo_text($date . "아침\\n" . $final, 0);
+                echo_text($date . " 아침\\n" . $final, 0);
             end_msg(1);
         end_echo();
     }
+    
     else if ( strcmp($content, "점심") == false ) {
-        $final = getmeal2(0);
-        $date = date("Y-m-d", strtotime("+$days days"));
+        $final = getmeal2();
         start_echo();
             start_msg();
-                echo_text($date . "점심\\n" . $final, 0);
+                echo_text($date . " 점심\\n" . $final, 0);
             end_msg(1);
         end_echo();
     }
+
     else if ( strcmp($content, "저녁") == false ) {
-        $final = getmeal3(0);
-        $date = date("Y-m-d", strtotime("+$days days"));
+        $final = getmeal3();
         start_echo();
             start_msg();
-                echo_text($date . "저녁\\n" . $final, 0);
+                echo_text($date . " 저녁\\n" . $final, 0);
             end_msg(1);
         end_echo();
     }
+
     else if ( strcmp($content, "간식") == false ) {
-        $final = getmeal4(0);
-        $date = date("Y-m-d", strtotime("+$days days"));
+        $final = getmeal4();
         start_echo();
             start_msg();
-                echo_text($date . "간식\\n" . $final, 0);
+                echo_text($date . " 간식\\n" . $final, 0);
             end_msg(1);
         end_echo();
     }
