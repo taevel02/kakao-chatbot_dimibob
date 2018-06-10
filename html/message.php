291619f70148bd9e3777f950106a1a071262ceed
Taehoon <kth020315@naver.com>
2018-06-10 16:46:33 +0900

    complete

--- html/message.php
+++ html/message.php
@@ -6,42 +6,49 @@ $header = array(
   'Content-type: application/json',
 );
 
-$url = "'https://api.dimigo.in/dimibobs/today'";
+$url = 'https://api.dimigo.in/dimibobs/today';
 
+// Open connection
 $ch = curl_init();
+// Set the url, number of GET vars, GET data
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, false);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
+// Execute request
 $result = curl_exec($ch);
+// Close connection
 curl_close($ch);
 
 $result_arr = json_decode($result, true);
 
 $breakfast = $result_arr['breakfast'];
-#if ($breakfast == null) $breakfast_show = "급식 정보가 없습니다.";
-#else $breakfast_show = "오늘 아침은 $breakfast 입니다.";
+if ($breakfast == null) $breakfast_show = "급식 정보가 없습니다.";
+else $breakfast_show = "오늘 아침은 $breakfast 입니다.";
 
 $lunch = $result_arr['lunch'];
-#if ($lunch == null) $lunch_show = "급식 정보가 없습니다.";
-#else $lunch_show = "오늘 점심은 $lunch 입니다.";
+if ($lunch == null) $lunch_show = "급식 정보가 없습니다.";
+else $lunch_show = "오늘 점심은 $lunch 입니다.";
 
 $dinner = $result_arr['dinner'];
-#if ($dinner == null) $dinner_show = "급식 정보가 없습니다.";
-#else $dinner_show = "오늘 저녁은 $dinner 입니다.";
+if ($dinner == null) $dinner_show = "급식 정보가 없습니다.";
+else $dinner_show = "오늘 저녁은 $dinner 입니다.";
 
 $snack = $result_arr['snack'];
-#if ($snack == null) $snack_show = "간식 정보가 없습니다.";
-#else $snack_show = "오늘 간식은 $snack 입니다."
+if ($snack == null) $snack_show = "간식 정보가 없습니다.";
+else $snack_show = "오늘 간식은 $snack 입니다.";
 
 
 if ($content == "아침") {
 echo <<< EOD
   {
     "message": {
-         "text": "'$breakfast'"
+        "text": "$breakfast_show"
+    },
+    "keyboard": {
+        "type": "buttons",
+        "buttons": ["아침", "점심", "저녁", "간식"]
     }
   }
 EOD;
@@ -51,7 +58,11 @@ else if ($content == "점심")  {
 echo <<< EOD
   {
     "message": {
-         "text": "$lunch"
+         "text": "$lunch_show"
+    },
+    "keyboard": {
+        "type": "buttons",
+        "buttons": ["아침", "점심", "저녁", "간식"]
     }
   }
 EOD;
@@ -61,7 +72,11 @@ else if ($content == "저녁") {
 echo <<< EOD
   {
     "message": {
-         "text": "$dinner"
+         "text": "$dinner_show"
+    },
+    "keyboard": {
+        "type": "buttons",
+        "buttons": ["아침", "점심", "저녁", "간식"]
     }
   }
 EOD;
@@ -71,7 +86,11 @@ else if ($content == "간식") {
 echo <<< EOD
   {
     "message": {
-         "text": "$snack"
+         "text": "$snack_show"
+    },
+    "keyboard": {
+        "type": "buttons",
+        "buttons": ["아침", "점심", "저녁", "간식"]
     }
   }
 EOD;
