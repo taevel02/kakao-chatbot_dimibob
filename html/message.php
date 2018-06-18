e2040b809cafd416af40f3b77d2278f6149b0cda
Taehoon <kth020315@naver.com>
2018-06-18 11:31:02 +0900

    Add tomorrow

--- html/message.php
+++ html/message.php
@@ -10,6 +10,12 @@ $date = date("Ymd");
 $date_show = date("m월 d일");
 $url = 'https://api.dimigo.in/dimibobs/' . $date;
 
+$tomorrow = date("Ymd", mktime(0, 0, 0, date("m"), date("d")+1, date("Y")));
+$tomorrow_show = date("m월 d일", mktime(0, 0, 0, date("m"), date("d")+1, date("Y")));
+$url2 = 'https://api.dimigo.in/dimibobs/' . $tomorrow;
+
+
+
 // Open connection
 $ch = curl_init();
 // Set the url, number of GET vars, GET data
@@ -42,6 +48,26 @@ if ($snack == null) $snack_show = "간식 정보가 없습니다.";
 else $snack_show = "오늘($date_show) 간식은 $snack 입니다.";
 
 
+
+// Open connection
+$ch2 = curl_init();
+// Set the url, number of GET vars, GET data
+curl_setopt($ch2, CURLOPT_URL, $url2);
+curl_setopt($ch2, CURLOPT_POST, false);
+curl_setopt($ch2, CURLOPT_HTTPHEADER, $header);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
+// Execute request
+$result = curl_exec($ch2);
+// Close connection
+curl_close($ch2);
+
+$result_arr = json_decode($result, true);
+
+$breakfast2 = $result_arr['breakfast'];
+if ($breakfast2 == null) $breakfast2_show = "급식 정보가 없습니다.";
+else $breakfast2_show = "내일($tomorrow_show) 아침은 $breakfast2 입니다.";
+
 if ($content == "아침") {
 echo <<< EOD
   {
@@ -50,7 +76,7 @@ echo <<< EOD
     },
     "keyboard": {
         "type": "buttons",
-        "buttons": ["아침", "점심", "저녁", "간식"]
+        "buttons": ["아침", "점심", "저녁", "간식", "내일 아침"]
     }
   }
 EOD;
@@ -64,7 +90,7 @@ echo <<< EOD
     },
     "keyboard": {
         "type": "buttons",
-        "buttons": ["아침", "점심", "저녁", "간식"]
+        "buttons": ["아침", "점심", "저녁", "간식", "내일 아침"]
     }
   }
 EOD;
@@ -78,7 +104,7 @@ echo <<< EOD
     },
     "keyboard": {
         "type": "buttons",
-        "buttons": ["아침", "점심", "저녁", "간식"]
+        "buttons": ["아침", "점심", "저녁", "간식", "내일 아침"]
     }
   }
 EOD;
@@ -92,7 +118,21 @@ echo <<< EOD
     },
     "keyboard": {
         "type": "buttons",
-        "buttons": ["아침", "점심", "저녁", "간식"]
+        "buttons": ["아침", "점심", "저녁", "간식", "내일 아침"]
+    }
+  }
+EOD;
+}
+
+else if ($content == "내일 아침") {
+echo <<< EOD
+  {
+    "message": {
+         "text": "$breakfast2_show"
+    },
+    "keyboard": {
+        "type": "buttons",
+        "buttons": ["아침", "점심", "저녁", "간식", "내일 아침"]
     }
   }
 EOD;
